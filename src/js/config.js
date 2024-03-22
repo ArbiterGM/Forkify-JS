0c97981ba8490933b6e669daeabbbaa239d01d0d
ArbiterGM <123399265+ArbiterGM@users.noreply.github.com>
2024-03-22 12:01:17 +0400

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "621c95f4-e665-4d2b-9052-baa280d02c80";
+export const MODAL_CLOSE_SEC = 2500;
